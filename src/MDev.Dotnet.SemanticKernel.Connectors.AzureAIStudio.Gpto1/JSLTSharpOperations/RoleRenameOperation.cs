b24606f500293aeacb1e5047b9bba5701b8857b4
MACK Mathieu <mathieu.mack@outlook.fr>
2024-11-26 17:47:17 +0100

    Feature/4 net9 (#6)
    * Ugrade to .net9
    Fix potential async issue (async operation without await statement
    
    * Start adding support GPT o1 models.
    Beware that it should e used in production !! API is in preview and there are some limitations :
    > System messages are transformed as User messages
    > Temperature is overrided wih 1
    > TopP is overrided with 1
    
    * Add exerimental attributes on functions
    
    * Remove completly phi3 (now supported natively by AIInference connector)
    Update service collection registry with JsonTransform to use Singletons
    
    * Fix: Register AzureOpenAIHttpCLientHandler as scoped
    
    ---------
    
    Co-authored-by: Abdelhalim Belaoud <abdelhalim.belaoud@viseo.com>

---
+++ src/MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1/JSLTSharpOperations/RoleRenameOperation.cs
@@ -0,0 +1,20 @@
+﻿using JSLTSharp.JsonTransforms.Abstractions;
+using Newtonsoft.Json.Linq;
+
+namespace MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1.JSLTSharpOperations;
+
+internal class RoleRenameOperation : IJsonTransformCustomOperation
+{
+    /// <inheritdoc />
+    public virtual string OperationName => "RenameRole";
+
+    /// <inheritdoc />
+    public virtual JToken Apply(JToken dataSource, JToken objectToApplyTo, IList<string> parameters)
+    {
+        if (objectToApplyTo.Type != JTokenType.String)
+            return objectToApplyTo;
+
+        var value = objectToApplyTo.ToString();
+        return value == "system" ? "user" : value;
+    }
+}
---
+++ src/MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1/ServiceCollectionExtensions/AIStudioMaaSGPTo1Extensions.cs
@@ -0,0 +1,32 @@
+﻿using JSLTSharp;
+using JSLTSharp.JsonTransforms.Extensions;
+using JSLTSharp.JsonTransforms.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1.JSLTSharpOperations;
+using MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1.HttpClientHandlers;
+
+namespace MDev.Dotnet.SemanticKernel.Connectors.AzureAIStudio.Gpto1.ServiceCollectionExtensions;
+
+public static class AIStudioMaaSGPTo1Extensions
+{
+    /// <summary>
+    /// Register the custom http client to interact with Azure OpenAI o1 models
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="httpClientName">The name of the custom http client</param>
+    /// <returns></returns>
+    public static IServiceCollection RegisterAIStudioMaasGPTo1(this IServiceCollection services, string httpClientName)
+    {
+        services.AddSingleton<IJsonTransformCustomOperation, RoleRenameOperation>();
+        services.AddScoped<AzureOpenAIHttpClientHandler>();
+        services.AddSingleton<JsonTransform>();
+        
+        services.AddHttpClient(httpClientName)
+            .ConfigureHttpClient((provider, client) =>
+            {
+            })
+       .AddHttpMessageHandler<AzureOpenAIHttpClientHandler>();
+
+        return services;
+    }
+}
